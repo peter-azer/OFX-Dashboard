94dbac469f01ecfaad61993871b31f6a29331ec4
peterazermosa1988 <peterazermosa1988@gmail.com>
2025-10-22 14:06:06 +0000

    admin settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,19 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
         $this->call([
-            UserSeeder::class,
+            AdminUserSeeder::class,
         ]);
     }
 }
